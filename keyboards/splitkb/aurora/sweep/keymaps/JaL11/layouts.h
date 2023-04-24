5bc77f13415c22e27d88728df346b60317fe8b95
JaL11 <52463133+JaL11@users.noreply.github.com>
2023-04-24 11:56:48 +0200

    [sweep] homerow mods

--- keyboards/splitkb/aurora/sweep/keymaps/JaL11/layouts.h
+++ keyboards/splitkb/aurora/sweep/keymaps/JaL11/layouts.h
@@ -1,11 +1,23 @@
 #pragma once
 
-// home-row mods (default)
-#define HM_A LCTL_T(KC_A)
-#define HM_S LGUI_T(KC_S)
-#define HM_D LALT_T(KC_D)
-#define HM_F LSFT_T(KC_F)
-#define HM_J RSFT_T(KC_J)
-#define HM_K RALT_T(KC_K)
-#define HM_L RGUI_T(KC_L)
-#define HM_QUOT RCTL_T(KC_QUOT)
+// home-row mods (default) CGAS
+// #define HM_A LCTL_T(KC_A)
+// #define HM_S LGUI_T(KC_S)
+// #define HM_D LALT_T(KC_D)
+// #define HM_F LSFT_T(KC_F)
+// #define HM_J RSFT_T(KC_J)
+// #define HM_K RALT_T(KC_K)
+// #define HM_L RGUI_T(KC_L)
+// #define HM_QUOT RCTL_T(KC_SCLN)
+
+// mods GASC
+#define HM_A LGUI_T(KC_A)
+#define HM_S LALT_T(KC_S)
+#define HM_D LSFT_T(KC_D)
+#define HM_F LCTL_T(KC_F)
+#define HM_J RCTL_T(KC_J)
+#define HM_K RSFT_T(KC_K)
+#define HM_L RALT_T(KC_L)
+#define HM_SCLN RGUI_T(KC_SCLN)
+
+
